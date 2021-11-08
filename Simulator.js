b3f1a24d9d0b3d5d9507bf723fae6d35114eb7bf
Muhammad Munawwar Adam <ma04289@st.habib.edu.pk>
2021-11-08 19:53:38 +0500

    Infinte Terrain Generation

--- Simulator.js
+++ Simulator.js
@@ -7,23 +7,34 @@ xmin = -5;
 xmax = 5;
 zmin = -5;
 zmax = 5;
-var speed = vec3(0, 0, -0.001);
+var speed = vec3(0, 0, -0.01);
 
 // Cordinate Arrays
 let vertices = [];
 let colors = [];
 let normals = [];
 // Perspective Projection
-let near = 0.1;
-let far = 100.0;
+let near = -1;
+let far = 1;
 let fov = 45;
 let aspect;
 var radius = 6.0;
 var theta = 55.0;
 var phi = 50;
 // Model View Project
-var eye = vec3(0, 1, 0);
-var up = vec3(0.0, 1.0, 0.0);
+let eye = vec3(0.0, 1.0, 0.0);
+let up = vec3(0.0, 1.0, 0.0);
+let look_at;
+let direction;
+let p;
+let mv;
+let projLoc;
+let mvLoc;
+// Controls for Infinite terrain Generation
+let center = vec2(0, 0);
+let patchsize = 5;
+let camera;
+let depth = 0.1;
 
 window.onload = function init() {
   canvas = document.getElementById("gl-canvas");
@@ -50,8 +61,8 @@ function get_patch(minX, maxX, minZ, maxZ) {
   let dx = 0.2;
   let dz = 0.2;
 
-  for (let x = xmin; x <= xmax; x += dx) {
-    for (let z = zmin; z <= zmax; z += dz) {
+  for (let x = minX; x <= maxX; x += dx) {
+    for (let z = minZ; z <= maxZ; z += dz) {
       // Triangle 1
       // (x,z) (x,z+dz),(x+dx,z+dz)
 
@@ -85,7 +96,24 @@ function get_height() {
   }
 }
 
+function update_terrain() {
+  let camera = vec2(eye[0], eye[2]);
+  let distFromPatch = length(subtract(camera, center));
+  if (depth * patchsize < distFromPatch) {
+    center = camera;
+    xmin = Math.floor(-patchsize + center[0]);
+    xmax = Math.floor(patchsize + center[0]);
+    zmin = Math.floor(-patchsize + center[1]);
+    zmax = Math.floor(patchsize + center[1]);
+
+    vertices = get_patch(xmin, xmax, zmin, zmax);
+    get_height();
+    load_buffer();
+  }
+}
+
 function load_buffer() {
+  gl.flush();
   gl.bindBuffer(gl.ARRAY_BUFFER, gl.createBuffer());
   gl.bufferData(gl.ARRAY_BUFFER, flatten(vertices), gl.STATIC_DRAW);
 
@@ -99,23 +127,19 @@ function load_buffer() {
   gl.vertexAttribPointer(colorLoc, 4, gl.FLOAT, false, 0, 0);
   gl.enableVertexAttribArray(colorLoc);
 }
-var look_at;
-function render() {
-  var direction = normalize(new vec3(speed));
-  console.log(look_at);
 
+function render() {
+  direction = normalize(new vec3(speed));
   look_at = add(eye, direction);
   gl.clear(gl.DEPTH_BUFFER_BIT | gl.COLOR_BUFFER_BIT);
-  let projLoc = gl.getUniformLocation(program, "p");
-  let mvLoc = gl.getUniformLocation(program, "mv");
-  let mv = lookAt(eye, look_at, up);
-  console.log(mv);
-  gl.uniformMatrix4fv(mvLoc, gl.FALSE, flatten(mv));
-  let p = perspective(fov, canvas.width / canvas.height, near, far);
+  projLoc = gl.getUniformLocation(program, "p");
+  mvLoc = gl.getUniformLocation(program, "mv");
+  mv = lookAt(eye, look_at, up);
+  p = perspective(fov, canvas.width / canvas.height, near, far);
   gl.uniformMatrix4fv(projLoc, gl.FALSE, flatten(p));
+  gl.uniformMatrix4fv(mvLoc, gl.FALSE, flatten(mv));
   gl.drawArrays(gl.LINES, 0, vertices.length);
-
   eye = add(speed, eye);
-
+  update_terrain();
   requestAnimationFrame(render);
 }
