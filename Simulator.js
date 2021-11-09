2a3d2d41be6dded86ec404badeadaa0dc089d04a
munawwar22HU <54769842+munawwar22HU@users.noreply.github.com>
2021-11-09 22:39:03 +0500

    Overwrite Incorrect Files.

--- Simulator.js
+++ Simulator.js
@@ -1,3 +1,257 @@
+<<<<<<< HEAD
+"use strict";
+// WebGL Variables
+let program, canvas, gl;
+// Terrain Bounds
+let xmin, xmax, zmin, zmax;
+xmin = -5;
+xmax = 5;
+zmin = -5;
+zmax = 5;
+var speed = vec3(0, 0.0, -0.01);
+let keyMaps;
+
+// Cordinate Arrays
+let vertices = [];
+let colors = [];
+let normals = [];
+// Perspective Projection
+
+let fov = 45;
+let aspect;
+var radius = 6.0;
+var theta = 55.0;
+var phi = 50;
+// Model View Project
+
+let eye = vec3(0.0, 1.0, 0.0);
+let up = vec3(0.0, 1.0, 0.0);
+let look_at = vec3(0.0, 0.0, 0.0);
+let at_vec = vec3(0.0, -0.2, -0.5);
+let direction;
+let p;
+let mv;
+let normalMat;
+let projLoc;
+let mvLoc;
+let normLoc;
+// Controls for Infinite terrain Generation
+let center = vec2(0, 0);
+let patchsize = 5;
+let camera;
+let depth = 0.1;
+
+let sTop = 1;
+let sright = 1;
+let sfar = -1;
+
+let sbottom = -1;
+let sleft = -1;
+let snear = 1;
+
+let mode = 0;
+let wiremode = 0;
+// 0 
+
+window.onload = function init() {
+  canvas = document.getElementById("gl-canvas");
+  gl = canvas.getContext("webgl2");
+  if (!gl) alert("WebGL 2.0 isn't availaible");
+
+  gl.viewport(0, 0, canvas.clientWidth, canvas.clientHeight);
+  gl.clearColor(0.0, 0.0, 0.0, 1.0);
+
+  gl.enable(gl.DEPTH_TEST);
+
+  if (mode == 0) {
+    program = initShaders(gl, "vertex-shader-f", "fragment-shader-f");
+  }
+
+  if (mode == 1) {
+    program = initShaders(gl, "vertex-shader-s", "fragment-shader-s");
+  }
+  if (mode == 2) {
+    program = initShaders(gl, "vertex-shader-p", "fragment-shader-p");
+  }
+
+
+
+  gl.useProgram(program);
+
+  vertices = get_patch(xmin, xmax, zmin, zmax);
+
+  document.onkeydown = keyPressHandler;
+  document.onkeyup = keyReleaseHandler;
+
+  for (let i = 0; i < vertices.length; i++) {
+    normals = vec3(0.0, 0.0, 0.0);
+  }
+  get_height();
+  load_buffer();
+  render();
+};
+
+function get_patch(minX, maxX, minZ, maxZ) {
+  let ret = [];
+  let dx = 0.05;
+  let dz = 0.05;
+
+  for (let x = minX; x <= maxX; x += dx) {
+    for (let z = minZ; z <= maxZ; z += dz) {
+      // Triangle 1
+      // (x,z) (x,z+dz),(x+dx,z+dz)
+
+      ret.push(vec4(x, 0, z, 1));
+      ret.push(vec4(x + dx, 0, z + dz, 1));
+      ret.push(vec4(x + dx, 0, z, 1));
+
+      ret.push(vec4(x, 0, z, 1));
+      ret.push(vec4(x, 0, z + dz, 1));
+      ret.push(vec4(x + dx, 0, z + dz, 1));
+      // Triangle 2
+      // (x,z) (x+dx,z),(x+dx,z+dz)
+
+      colors.push(vec4(1.0, 1.0, 1.0, 1.0));
+      colors.push(vec4(1.0, 1.0, 1.0, 1.0));
+
+      colors.push(vec4(1.0, 1.0, 1.0, 1.0));
+      colors.push(vec4(1.0, 1.0, 1.0, 1.0));
+
+      colors.push(vec4(1.0, 1.0, 1.0, 1.0));
+      colors.push(vec4(1.0, 1.0, 1.0, 1.0));
+    }
+  }
+  return ret;
+}
+
+function get_height() {
+  noise.seed(42);
+  for (let i = 0; i < vertices.length; i++) {
+    let res = noise.perlin2(vertices[i][0], vertices[i][2]);
+    let height = res[0];
+    vertices[i][1] = height //> 0 ? height : 0;
+    normals[i] = res[1];;
+  }
+}
+
+function update_terrain() {
+  let camera = vec2(eye[0], eye[2]);
+  let distFromPatch = length(subtract(camera, center));
+  if (depth * patchsize < distFromPatch) {
+    center = camera;
+    xmin = Math.floor(-patchsize + center[0]);
+    xmax = Math.floor(patchsize + center[0]);
+    zmin = Math.floor(-patchsize + center[1]);
+    zmax = Math.floor(patchsize + center[1]);
+    colors = [];
+    normals = [];
+    vertices = get_patch(xmin, xmax, zmin, zmax);
+    get_height();
+    load_buffer();
+  }
+}
+
+function load_buffer() {
+  gl.flush();
+  gl.bindBuffer(gl.ARRAY_BUFFER, gl.createBuffer());
+  gl.bufferData(gl.ARRAY_BUFFER, flatten(vertices), gl.STATIC_DRAW);
+
+  let positionLoc = gl.getAttribLocation(program, "vPosition");
+  gl.vertexAttribPointer(positionLoc, 4, gl.FLOAT, false, 0, 0);
+  gl.enableVertexAttribArray(positionLoc);
+
+  gl.bindBuffer(gl.ARRAY_BUFFER, gl.createBuffer());
+  gl.bufferData(gl.ARRAY_BUFFER, flatten(normals), gl.STATIC_DRAW);
+
+  if (mode != 2) {
+    let normalLoc = gl.getAttribLocation(program, "Vnormal");
+    gl.vertexAttribPointer(normalLoc, 3, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(normalLoc);
+  }
+
+  gl.bindBuffer(gl.ARRAY_BUFFER, gl.createBuffer());
+  gl.bufferData(gl.ARRAY_BUFFER, flatten(colors), gl.STATIC_DRAW);
+  let colorLoc = gl.getAttribLocation(program, "uColor");
+  gl.vertexAttribPointer(colorLoc, 4, gl.FLOAT, false, 0, 0);
+  gl.enableVertexAttribArray(colorLoc);
+}
+
+function render() {
+  update_terrain();
+  // console.log(up);
+  eye = add(eye, speed);
+
+  look_at = add(eye, at_vec);
+  gl.clear(gl.DEPTH_BUFFER_BIT | gl.COLOR_BUFFER_BIT);
+
+  projLoc = gl.getUniformLocation(program, "p");
+  mv = lookAt(eye, look_at, up);
+  mvLoc = gl.getUniformLocation(program, "mv");
+  if (mode != 1) {
+    normLoc = gl.getUniformLocation(program, "normMat");
+    normalMat = normalMatrix(mv, false);
+    gl.uniformMatrix3fv(normLoc, gl.FALSE, flatten(normalMat));
+  }
+  p = frustum(sleft, sright, sbottom, sTop, snear, sfar);
+
+  gl.uniformMatrix4fv(projLoc, gl.FALSE, flatten(p));
+  gl.uniformMatrix4fv(mvLoc, gl.FALSE, flatten(mv));
+
+  if (wiremode == 2) {
+
+    gl.drawArrays(gl.TRIANGLES, 0, vertices.length);
+
+  }
+
+  if (wiremode == 1) {
+    gl.drawArrays(gl.LINES, 0, vertices.length);
+  }
+
+  if (wiremode == 0) {
+    gl.drawArrays(gl.POINTS, 0, vertices.length);
+  }
+
+
+
+  requestAnimationFrame(render);
+}
+
+function frustum(left, right, bottom, top, near, far) {
+
+  if (left == right) { throw "frustum(): left and right are equal"; }
+
+  if (bottom == top) { throw "frustum(): bottom and top are equal"; }
+
+  if (near == far) { throw "frustum(): near and far are equal"; }
+
+  let w = right - left;
+
+  let h = top - bottom;
+
+  let d = far - near;
+
+  let result = mat4();
+
+  result[0][0] = 2.0 * near / w;
+
+  result[1][1] = 2.0 * near / h;
+
+  result[2][2] = -(far + near) / d;
+
+  result[0][2] = (right + left) / w;
+
+  result[1][2] = (top + bottom) / h;
+
+  result[2][3] = -2 * far * near / d;
+
+  result[3][2] = -1;
+
+  result[3][3] = 0.0;
+
+  return result;
+
+}
+=======
 "use strict";
 // WebGL Variables
 let program, canvas, gl;
@@ -247,3 +501,4 @@ function frustum(left, right, bottom, top, near, far) {
   return result;
 
 }
+>>>>>>> 0807bbf7d011729855190d4fce376a4423db16a8
--- control.js
+++ control.js
@@ -1,4 +1,6 @@
 keyMaps = {
+  V: false,
+  C: false,
   W: false, //Pitch up
   S: false, //Pitch down
   A: false, //Yaw left
@@ -17,8 +19,8 @@ keyMaps = {
   "#": false, //top
   4: false,
   $: false, //bottom
-  ArrowUp: false, //Increase speed
-  ArrowDown: false, //Increase down
+  'ARROWUP': false, //Increase speed
+  'ARROWDOWN': false, //Increase down
 };
 
 const keyPressHandler = (e) => {
@@ -33,6 +35,32 @@ const keyReleaseHandler = (e) => {
 };
 
 const updateView = () => {
+  if (keyMaps["V"] === true) {
+    if (mode == 2) {
+      wiremode = 2
+    }
+    else {
+      if (wiremode == 2) {
+        wiremode = 0;
+      }
+      else {
+        wiremode += 1;
+      }
+    }
+
+  }
+  if (keyMaps["C"] === true) {
+    if (mode == 2) {
+      mode = 0;
+    }
+    else {
+      mode += 1;
+    }
+
+    if (mode == 2) {
+      wiremode = 2;
+    }
+  }
   if (keyMaps["W"] === true) {
     at_vec[1] += 0.03;
   }
@@ -42,64 +70,134 @@ const updateView = () => {
   if (keyMaps["Q"] === true) {
     if (up[0] > -1.0) {
       up[0] -= 0.03;
-      console.log(up);
+      // console.log(up);
     }
   }
   if (keyMaps["E"] === true) {
     if (up[0] <= 1.0) {
       up[0] += 0.03;
-      console.log(up);
+      // console.log(up);
     }
   }
+
+
+
+
+
+
   if (keyMaps["A"] === true) {
+
     at_vec[0] -= 0.03;
-    speed[0] -= 0.03;
+
+
+
+
+
+    if (at_vec[0] > 0) {
+      speed[0] -= at_vec[0] / 1000;
+      // if (speed[0] <= -0.05) {speed[0] = -0.05}
+
+      speed[2] = speed[2] - Math.abs(speed[0])
+      if (speed[2] <= -0.05) { speed[2] = -0.05 }
+    }
+    else {
+
+      speed[0] = speed[0] + at_vec[0] / 1000;
+      // if (speed[0] <= -0.05) {speed[0] = -0.05}
+
+      speed[2] = speed[2] + Math.abs(speed[0])
+      if (speed[2] > 0) { speed[2] = 0 }
+    }
+
   }
+
+
+
+
+
+  console.log(speed)
+  // console.log(at_vec);
   if (keyMaps["D"] === true) {
     at_vec[0] += 0.03;
-    speed[0] += 0.03;
+
+    if (at_vec[0] <= 0) {
+      speed[0] = speed[0] - at_vec[0] / 1000;
+      if (speed[0] >= 0.05) { speed[0] = 0.05 }
+
+      speed[2] = speed[2] - Math.abs(speed[0])
+      if (speed[2] <= -0.05) { speed[2] = -0.05 }
+    }
+    else {
+      speed[0] = speed[0] + at_vec[0] / 1000;
+      // if (speed[0] >= 0.05){speed[0] = 0.05}
+
+      speed[2] = speed[2] + Math.abs(speed[0])
+      if (speed[2] > 0) { speed[2] = 0 }
+    }
+
   }
-  
+
+
+
+
   if (keyMaps['1']) {
-    if (sleft < -0.5) { sleft += 0.05}
-}
-if (keyMaps['!']) {
-    if (sleft > -2) { sleft -= 0.05}
-}
+    if (sleft < -0.5) { sleft += 0.05 }
+  }
+  if (keyMaps['!']) {
+    if (sleft > -2) { sleft -= 0.05 }
+  }
 
-if (keyMaps['2']) {
-    if (sright > 0.5) { sright -= 0.05}
-}
-if (keyMaps['@']) {
-    if (sright < 2) { sright += 0.05}
-}
+  if (keyMaps['2']) {
+    if (sright > 0.5) { sright -= 0.05 }
+  }
+  if (keyMaps['@']) {
+    if (sright < 2) { sright += 0.05 }
+  }
 
-if (keyMaps['3']) {
-    if (sTop > 0.5) { sTop -= 0.05}
-}
-if (keyMaps['#']) {
-    if (sTop < 2) { sTop += 0.05}
-}
+  if (keyMaps['3']) {
+    if (sTop > 0.5) { sTop -= 0.05 }
+  }
+  if (keyMaps['#']) {
+    if (sTop < 2) { sTop += 0.05 }
+  }
 
-if (keyMaps['4']) {
-    if (sbottom < -0.5) { sbottom += 0.05}
-}
-if (keyMaps['$']) {
-    if (sbottom > -2) { sbottom -= 0.05}
-}
+  if (keyMaps['4']) {
+    if (sbottom < -0.5) { sbottom += 0.05 }
+  }
+  if (keyMaps['$']) {
+    if (sbottom > -2) { sbottom -= 0.05 }
+  }
 
 
-if (keyMaps['5']) {
-    if (snear > 0.5) { snear -= 0.05}
-}
-if (keyMaps['%']) {
-    if (snear < 2) { snear -= 0.05}
-}
+  if (keyMaps['5']) {
+    if (snear > 0.5) { snear -= 0.05 }
+  }
+  if (keyMaps['%']) {
+    if (snear < 2) { snear += 0.05 }
+  }
 
-if (keyMaps['6']) {
-    if (sfar < -0.5) { sfar += 0.05}
-}
-if (keyMaps['^']) {
-    if (sfar > -2) { sfar -= 0.05}
-}
+  if (keyMaps['6']) {
+    if (sfar < -0.5) { sfar += 0.05 }
+  }
+  if (keyMaps['^']) {
+    if (sfar > -2) { sfar -= 0.05 }
+  }
+
+  if (keyMaps['ARROWUP']) {
+    // console.log('hi')
+    if (acc <= 2) {
+      acc += 0.1;
+      speed = vec3(speed[0] + acc * speed[0] * 0.3, speed[1] + acc * speed[1] * 0.3, speed[2] + acc * speed[2] * 0.3)
+      console.log(speed);
+    }
+  }
+
+  if (keyMaps['ARROWDOWN']) {
+    // console.log('hi')
+    if (acc >= 1) {
+      acc -= 0.1;
+      speed = vec3(speed[0] - acc * speed[0] * 0.2, speed[1] - acc * speed[1] * 0.2, speed[2] - acc * speed[2] * 0.2)
+      console.log(speed);
+    }
+  }
 };
